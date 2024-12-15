7a217476097510a82a3d5028f3ed5aabd2c99724
Davs06 <santosdv40@gmail.com>
2024-12-15 14:29:38 -0300

    Criadno rotas para os layouts

--- app-vue/src/router/index.js
+++ app-vue/src/router/index.js
@@ -1,21 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import BaseTemplate from '@/layouts/BaseTemplate.vue'
+import AuthTemplate from '@/layouts/AuthTemplate.vue'
 import HomeView from '../views/HomeView.vue'
+import LogonView from '@/views/auth/LogonView.vue'
+import LoginView from '@/views/auth/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+
       path: '/',
-      name: 'home',
-      component: HomeView,
+      component: BaseTemplate,
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomeView,
+        }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/',
+      component: AuthTemplate,
+      children: [
+        {
+          path: '/login',
+          name: 'login',
+          component: LoginView
+        },
+        {
+          path: '/logon',
+          name: 'logon',
+          component: LogonView
+        }
+      ]
     },
   ],
 })
