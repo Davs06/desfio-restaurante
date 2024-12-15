02eb1ba5cef66f38c2acae0c48dd17af006befd3
Davs06 <santosdv40@gmail.com>
2024-12-15 14:58:49 -0300

    view de autenticação

---
+++ app-vue/src/components/auth/CardAuth.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="container d-flex justify-content-center my-5">
+    <div class="card">
+      <div class="card-header">
+        <h3>{{ title }}</h3>
+      </div>
+      <div class="card-body">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ app-vue/src/views/auth/LoginView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import CardAuth from "@/components/auth/CardAuth.vue";
+import FormLogin from "@/components/auth/FormLogin.vue";
+
+const title = "Login";
+</script>
+
+<template>
+  <div>
+    <CardAuth :title="title">
+      <FormLogin />
+    </CardAuth>
+  </div>
+</template>
