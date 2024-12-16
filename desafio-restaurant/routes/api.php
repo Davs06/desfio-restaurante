c67f8f3106e02a351dee10f311158705d8ab61a7
Davs06 <santosdv40@gmail.com>
2024-12-16 19:30:40 -0300

    Criando api

--- desafio-restaurant/routes/api.php
+++ desafio-restaurant/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('/mesa', \App\Http\Controllers\Api\MesaApiController::class);
+//} );
+
+Route::post('/login', [\App\Http\Controllers\Api\Auth\AuthController::class, 'login']);
+
+Route::ApiResource('/reserva', \App\Http\Controllers\Api\ReservaApiController::class);
+
+
+//Route::post('/create', [\App\Http\Controllers\Api\ReservaApiController::class, 'store']);
+//Route::put('/update/{reserva}', [\App\Http\Controllers\Api\ReservaApiController::class, 'update']);
+//Route::delete('/remove/{reserva}', [\App\Http\Controllers\Api\ReservaApiController::class, 'destroy']);
+
+
